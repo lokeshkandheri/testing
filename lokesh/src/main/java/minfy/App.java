003bc91b892ee037f6a34a720cf59f6db27a6c5b
Lokesh <lokeshkandheri1947@gmail.com>
2020-07-28 01:17:40 +0530

    Correct Commit

--- lokesh/src/main/java/minfy/App.java
+++ lokesh/src/main/java/minfy/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "My jenkins updated project" );
+        System.out.println( "My jenkins updated Correct project" );
     }
 }
